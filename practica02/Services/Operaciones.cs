6a82b0c3e784008eda6289ca8ea1b0be559ddf06
darien786 <darienbaltazar2@gmail.com>
2023-02-28 23:00:09 -0600

    pratica actualizada

--- practica02/Services/Operaciones.cs
+++ practica02/Services/Operaciones.cs
@@ -4,14 +4,34 @@ using WSDL.mensajes;
 namespace WSDL.operaciones{
     
     public class Operaciones : Mensajes{
-       
-        public string Saludar(string nombre){
-            string msj = "Hola "+ nombre;
-            return msj;
-        }
 
-        public string Mostrar(int id){
-            return "x";
+        private string[] mensajes = new string[4];
+
+        public string Saludar(string nombre){
+            Boolean estaLleno = true;
+            for(int x = 0; x < mensajes.Length; x++){
+                if(string.IsNullOrEmpty(mensajes[x])){
+                    mensajes[x]=nombre;
+                    estaLleno = false;
+                    break;
+                }
+            }
+
+            if (estaLleno == true){
+                return "Matriz llena";
+            }
+            return "Hola "+nombre;
         }
+        public string Mostrar(int id){
+            if(id >= mensajes.Length || id < 0){
+                return "No se encontro";
+            } 
+            
+            if(!string.IsNullOrEmpty(mensajes[id])){
+                return "Hola " + mensajes[id];
+            }
+            return "No se encontro";
+        }
+        
     }
 }
