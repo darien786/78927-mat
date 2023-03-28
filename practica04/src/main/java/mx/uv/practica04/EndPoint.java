960343bbb0b53908c0b49dba8be8259b7f1068cd
darien786 <darienbaltazar2@gmail.com>
2023-03-28 02:16:27 -0600

    funciones con base de datos

--- practica04/src/main/java/mx/uv/practica04/EndPoint.java
+++ practica04/src/main/java/mx/uv/practica04/EndPoint.java
@@ -1,5 +1,7 @@
 package mx.uv.practica04;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
@@ -13,6 +15,7 @@ import https.t4is_uv_mx.saludos.EliminarRequest;
 import https.t4is_uv_mx.saludos.EliminarResponse;
 import https.t4is_uv_mx.saludos.ModificarRequest;
 import https.t4is_uv_mx.saludos.ModificarResponse;
+import https.t4is_uv_mx.saludos.MostrarResponse;
 import https.t4is_uv_mx.saludos.SaludarRequest;
 
 @Endpoint
@@ -35,7 +38,7 @@ public class EndPoint{
         saludador.setNombre(peticion.getNombre());
         //meter a la base de datos
         iSaludador.save(saludador);
-
+        
         
         SaludarResponse respuesta = new SaludarResponse();
          for(int i=0; i<myArray.length;i++){
@@ -48,16 +51,23 @@ public class EndPoint{
         respuesta.setRespuesta("Hola " + peticion.getNombre() + ", mucho gusto");
         return respuesta;
     }
-    /*
+    
     @PayloadRoot (localPart="BuscarRequest", namespace="https://t4is.uv.mx/saludos")
     @ResponsePayload
     public BuscarResponse Buscar(@RequestPayload BuscarRequest posicion){
         BuscarResponse respuesta = new BuscarResponse();
-        if(!estaVacia(myArray,posicion.getPosicion())){
-            respuesta.setRespuesta("Hola " + myArray[posicion.getPosicion()].toString() + ", mucho gusto");
+        // if(!estaVacia(myArray,posicion.getPosicion())){
+        //     respuesta.setRespuesta("Hola " + myArray[posicion.getPosicion()].toString() + ", mucho gusto");
+        // }else{
+        //     respuesta.setRespuesta("No se encontro");    
+        // }
+
+        if(!iSaludador.findById(posicion.getPosicion()).isEmpty()){
+            respuesta.setRespuesta("Hola " + iSaludador.findById(posicion.getPosicion()).get().getNombre() + ", mucho gusto");
         }else{
-            respuesta.setRespuesta("No se encontro");    
+            respuesta.setRespuesta("No se encontro");
         }
+        
         return respuesta;
     }
 
@@ -65,9 +75,21 @@ public class EndPoint{
     @ResponsePayload
     public ModificarResponse Modificar(@RequestPayload ModificarRequest posicion, @RequestPayload ModificarRequest peticion){
     ModificarResponse respuesta = new ModificarResponse();
-    if(!estaVacia(myArray, posicion.getPosicion())){
-        myArray[posicion.getPosicion()] = peticion.getNombre();
+    Saludador saludador = new Saludador();
+    // if(!estaVacia(myArray, posicion.getPosicion())){
+    //     myArray[posicion.getPosicion()] = peticion.getNombre();
+    //     respuesta.setRespuesta("Saludo Modificado");
+    // }else{
+    //     respuesta.setRespuesta("No se encontro");
+    // }
+    
+    if(!iSaludador.findById(posicion.getPosicion()).isEmpty()){
+        saludador.setId(posicion.getPosicion());
+        saludador.setNombre(peticion.getNombre());
+        iSaludador.save(saludador);
+
         respuesta.setRespuesta("Saludo Modificado");
+    
     }else{
         respuesta.setRespuesta("No se encontro");
     }
@@ -78,11 +100,35 @@ public class EndPoint{
     @ResponsePayload
     public EliminarResponse  Eliminar(@RequestPayload EliminarRequest posicion){
         EliminarResponse respuesta = new EliminarResponse();
-        if(!estaVacia(myArray, posicion.getPosicion())){
-            myArray[posicion.getPosicion()] = null;
+        // if(!estaVacia(myArray, posicion.getPosicion())){
+        //     myArray[posicion.getPosicion()] = null;
+        //     respuesta.setRespuesta("Saludo eliminado");
+        // }
+
+        if(!iSaludador.findById(posicion.getPosicion()).isEmpty()){
+            iSaludador.deleteById(posicion.getPosicion());
             respuesta.setRespuesta("Saludo eliminado");
+        }else{
+            respuesta.setRespuesta("No se encontro");
+        }
+        return respuesta;
+    }
+
+    @PayloadRoot (localPart="MostrarRequest", namespace="https://t4is.uv.mx/saludos")
+    @ResponsePayload
+    public MostrarResponse Mostrar(){
+        MostrarResponse mostrar = new MostrarResponse();
+        String cadena = "";
+        List<Saludador> saludados = (List<Saludador>) iSaludador.findAll();
+        for(Saludador saludo : saludados){
+            
+            cadena += "ID: "+ saludo.getId().toString()+ ", NOMBRE: " + saludo.getNombre().toString() + ", ";
+            
+            mostrar.setRespuesta(cadena);
         }
 
-        return respuesta;
-    }*/
+        return mostrar;
+    }
+
+
 }
--- practica04/src/main/java/mx/uv/practica04/ISaludador.java
+++ practica04/src/main/java/mx/uv/practica04/ISaludador.java
@@ -1,8 +1,9 @@
 package mx.uv.practica04;
 
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface ISaludador extends CrudRepository<Saludador,Integer>{
-    
+  
     
 }
